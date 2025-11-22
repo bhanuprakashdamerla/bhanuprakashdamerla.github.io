a501d8b0ff8f822e23757f1bc8d638170ffcba92
bhanuprakash <bhanuprakashdamerla@gmail.com>
2025-11-22 13:46:37 +0530

    Feature: Implement NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "#components/Navbar"
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Macos_Portfolio</h1>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
